ea273fabbb5e2a3f4749037145178cf1ee544851
Nicole Sanders <nsanders9022@gmail.com>
2017-05-01 08:45:53 -0700

    add xunit testing (without a database)

---
+++ ToDoList.Tests/ControllerTests/ItemsControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Controllers;
+using ToDoList.Models;
+using Xunit;
+
+namespace ToDoList.Tests.ControllerTests
+{
+    public class ItemsControllerTest
+    {
+        //Checks to see if ToDoLIst.ItemsController.Index()
+        //method returns a valid MVC view
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            ItemsController controller = new ItemsController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        //checks to see if the model contains a list of items
+        [Fact]
+        public void Get_ModelList_Index_Test()
+        {
+            //Arrange
+            ItemsController controller = new ItemsController();
+            IActionResult actionResult = controller.Index();
+            ViewResult indexView = controller.Index() as ViewResult;
+
+            //above code could be refactored to ViewResult indexView = new ItemsController().Index() as ViewResul
+
+            //Act
+            var result = indexView.ViewData.Model;
+
+            //Assert
+            Assert.IsType<List<Item>>(result);
+        }
+    }
+}
---
+++ ToDoList.Tests/ItemTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Models;
+using Xunit;
+
+namespace ToDoList.Tests
+{
+    public class ItemTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var item = new Item();
+            item.Description = "Wash the dog";
+
+            //Act
+            var result = item.Description;
+
+            //Assert
+            Assert.Equal("Wash the dog", result);
+        }
+    }
+}
